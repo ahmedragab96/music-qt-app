1b4dfa42fefd3f200627887ff4fe00722d516c7f
ahmedragab96 <a.ragab@theelephant.tech>
2020-12-26 01:02:25 +0200

    add music table and fill it with data

--- musicapp.cpp
+++ musicapp.cpp
@@ -7,27 +7,44 @@ musicapp::musicapp(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QStringList drivers = QSqlDatabase::drivers();
-   foreach(QString driver, drivers)
-   qDebug()<<driver;
-
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("/home/ragab/Documents/musicapp.db");
 
-    if (db.open()) {
-        qDebug() << "Database is connected";
-        QSqlQuery qry;
-        qry.prepare("SELECT * FROM musicCDs");
-        if (qry.exec()) {
-            qDebug() << "exec qry";
-            while(qry.next()) {
-                qDebug() << qry.value(1).toString();
-            }
-        }
-    } else {
-        QMessageBox::information(this, "Connection", "Database is not connected");
+    if (!db.open()) {
+        QMessageBox::information(this, "Connection", "Error connecting to database");
     }
 
+    QSqlQuery query(db);
+    QString queryString = "SELECT * FROM musicCDs";
+    if (!query.exec(queryString)) {
+        qDebug() << "Failed";
+    }
+
+    ui->musicTable->setColumnCount(5);
+    QStringList labels;
+    labels << " " << "CD Name" << "Author Name" << "Genre" << "Year";
+    ui->musicTable->setHorizontalHeaderLabels(labels);
+
+    int rowCount = 0;
+    while(query.next()) {
+        ui->musicTable->insertRow(rowCount);
+        QTableWidgetItem *cd_name = new QTableWidgetItem;
+        QTableWidgetItem *author_name = new QTableWidgetItem;
+        QTableWidgetItem *genre = new QTableWidgetItem;
+        QTableWidgetItem *year = new QTableWidgetItem;
+
+        cd_name->setText(query.value(1).toString());
+        author_name->setText(query.value(2).toString());
+        genre->setText(query.value(3).toString());
+        year->setText(query.value(4).toString());
+
+        ui->musicTable->setItem(rowCount, 1, cd_name);
+        ui->musicTable->setItem(rowCount, 2, author_name);
+        ui->musicTable->setItem(rowCount, 3, genre);
+        ui->musicTable->setItem(rowCount, 4, year);
+
+        rowCount++;
+    }
 }
 
 musicapp::~musicapp()
